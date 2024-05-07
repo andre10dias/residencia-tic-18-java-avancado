fb5ae252483ff232bb2b758532bfb89c939bd229
andre10dias <andre10dias@ymail.com>
2024-05-07 12:16:14 -0300

    ap002

--- java-avancado/AP002/projeto/src/main/java/com/ecomerce/projeto/ProjetoApplication.java
+++ java-avancado/AP002/projeto/src/main/java/com/ecomerce/projeto/ProjetoApplication.java
@@ -2,12 +2,14 @@ package com.ecomerce.projeto;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.cache.annotation.EnableCaching;
 
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
 @EnableCaching
+//@SpringBootApplication(exclude={DataSourceAutoConfiguration.class})
 @SpringBootApplication
 public class ProjetoApplication {
 
---
+++ java-avancado/AP002/projeto/src/main/java/com/ecomerce/projeto/config/DataSourceConfig.java
@@ -0,0 +1,21 @@
+package com.ecomerce.projeto.config;
+
+import javax.sql.DataSource;
+
+import org.springframework.boot.jdbc.DataSourceBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+
+@Configuration
+public class DataSourceConfig {
+    @Bean
+    public DataSource getDataSource() {
+        return DataSourceBuilder.create()
+          .driverClassName("org.postgresql.Driver")
+          .url("jdbc:postgresql://localhost:5432/residencia18")
+          .username("postgres")
+          .password("admin")
+          .build();
+    }
+}
